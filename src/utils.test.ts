bf15490fb03090522b7919c410b1f47322147512
haiturtle <haiturtle@haiturtle.com>
2022-04-28 10:32:56 -0400

    IMPROVE: move test circle to outer block and add test case for true

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,17 +1,26 @@
 import { isPointInShape } from "./utils";
 describe("isPointInShape", () => {
+  const circle = {
+    x: 8,
+    y: 2,
+    radius: 2,
+  };
+
   it("returns false when point is outside of circle", () => {
     const x = 4;
     const y = 2;
 
-    const circle = {
-      x: 8,
-      y: 2,
-      radius: 2,
-    };
-
     const result = isPointInShape(x, y, circle);
 
     expect(result).toBe(false);
+  });
+
+  it("returns true when point is inside of circle", () => {
+    const x = 9;
+    const y = 3.5;
+
+    const result = isPointInShape(x, y, circle);
+
+    expect(result).toBe(true);
   });
 });
