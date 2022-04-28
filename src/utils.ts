a5fef4172c8b0ba645d1b0606a326bf0de4419f0
haiturtle <haiturtle@haiturtle.com>
2022-04-28 10:29:18 -0400

    FIX: failing point outside of circle test

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,12 @@
 import type { Shape } from "./shapes";
 
-export const isPointInShape = (x: number, y: number, s: Shape) => {
-  console.log("implement this function");
+export const isPointInShape = (x: number, y: number, s: Shape): boolean => {
+  if ("radius" in s) {
+    const d = Math.sqrt((s.x - x) ** 2 + (s.y - y) ** 2);
+
+    return d <= s.radius;
+  }
+
+  console.log("implement reactangle logic");
+  return true;
 };
