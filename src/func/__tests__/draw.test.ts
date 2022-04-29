dbe2530325ba7a038d14729211e9df31d997e6b9
haiturtle <haiturtle@haiturtle.com>
2022-04-29 07:43:08 -0400

    ADD: selectShape fxn and organize draw tests

--- src/func/__tests__/draw.test.ts
+++ src/func/__tests__/draw.test.ts
@@ -32,150 +32,329 @@ beforeEach(function () {
 });
 
 describe("drawShape", () => {
-  it("calls fillStyle method first when drawing a shape", () => {
-    expect.assertions(1);
+  describe("circle", () => {
+    it("calls fillStyle method first", () => {
+      expect.assertions(1);
 
-    if (!ctx) throw new Error("context was not mocked");
+      if (!ctx) throw new Error("context was not mocked");
 
-    drawShape(ctx, circle);
+      drawShape(ctx, circle);
 
-    // @ts-expect-error these methods on the mock dont exist on the real rendering context
-    const events: any[] = ctx.__getEvents();
+      // @ts-expect-error these methods on the mock dont exist on the real rendering context
+      const events: any[] = ctx.__getEvents();
 
-    expect(events[0]["type"]).toStrictEqual("fillStyle");
+      expect(events[0]["type"]).toStrictEqual("fillStyle");
+    });
+
+    it("calls beginPath method second", () => {
+      expect.assertions(1);
+
+      if (!ctx) throw new Error("context was not mocked");
+
+      drawShape(ctx, circle);
+
+      // @ts-expect-error these methods on the mock dont exist on the real rendering context
+      const events: any[] = ctx.__getEvents();
+
+      expect(events[1]["type"]).toStrictEqual("beginPath");
+    });
+
+    it("calls arc method third", () => {
+      expect.assertions(1);
+
+      if (!ctx) throw new Error("context was not mocked");
+
+      drawShape(ctx, circle);
+
+      // @ts-expect-error these methods on the mock dont exist on the real rendering context
+      const events: any[] = ctx.__getEvents();
+
+      expect(events[2]["type"]).toStrictEqual("arc");
+    });
+
+    it("calls arc method with expected props", () => {
+      expect.assertions(1);
+
+      const expected = {
+        x: circle.center.x,
+        y: circle.center.y,
+        radius: circle.radius,
+        startAngle: 0,
+        endAngle: 6.283185307179586,
+        anticlockwise: true,
+      };
+
+      if (!ctx) throw new Error("context was not mocked");
+
+      drawShape(ctx, circle);
+
+      // @ts-expect-error these methods on the mock dont exist on the real rendering context
+      const events: any[] = ctx.__getEvents();
+
+      expect(events[2]["props"]).toStrictEqual(expected);
+    });
+
+    it("calls fill method last", () => {
+      expect.assertions(1);
+
+      if (!ctx) throw new Error("context was not mocked");
+
+      drawShape(ctx, circle);
+
+      // @ts-expect-error these methods on the mock dont exist on the real rendering context
+      const events: any[] = ctx.__getEvents();
+
+      expect(events[events.length - 1]["type"]).toStrictEqual("fill");
+    });
+
+    it("calls ctx four times when drawing a circle", () => {
+      expect.assertions(1);
+
+      if (!ctx) throw new Error("context was not mocked");
+
+      drawShape(ctx, circle);
+
+      // @ts-expect-error these methods on the mock dont exist on the real rendering context
+      const events: any[] = ctx.__getEvents();
+
+      expect(events).toHaveLength(4);
+    });
   });
 
-  it("calls beginPath method second when drawing a circle", () => {
-    expect.assertions(1);
+  describe("rectangle", () => {
+    it("calls fillStyle method first when drawing a rectangle", () => {
+      expect.assertions(1);
 
-    if (!ctx) throw new Error("context was not mocked");
+      if (!ctx) throw new Error("context was not mocked");
 
-    drawShape(ctx, circle);
+      drawShape(ctx, rect);
 
-    // @ts-expect-error these methods on the mock dont exist on the real rendering context
-    const events: any[] = ctx.__getEvents();
+      // @ts-expect-error these methods on the mock dont exist on the real rendering context
+      const events: any[] = ctx.__getEvents();
 
-    expect(events[1]["type"]).toStrictEqual("beginPath");
-  });
+      expect(events[0]["type"]).toStrictEqual("fillStyle");
+    });
 
-  it("calls arc method third when drawing a circle", () => {
-    expect.assertions(1);
+    it("calls fillRect method second when drawing a rect", () => {
+      expect.assertions(1);
 
-    if (!ctx) throw new Error("context was not mocked");
+      if (!ctx) throw new Error("context was not mocked");
 
-    drawShape(ctx, circle);
+      drawShape(ctx, rect);
 
-    // @ts-expect-error these methods on the mock dont exist on the real rendering context
-    const events: any[] = ctx.__getEvents();
+      // @ts-expect-error these methods on the mock dont exist on the real rendering context
+      const events: any[] = ctx.__getEvents();
 
-    expect(events[2]["type"]).toStrictEqual("arc");
-  });
+      expect(events[1]["type"]).toStrictEqual("fillRect");
+    });
 
-  it("calls arc method with expected props when drawing a circle", () => {
-    expect.assertions(1);
+    it("calls fillRect method with expected props when drawing a rect", () => {
+      expect.assertions(1);
 
-    const expected = {
-      x: circle.center.x,
-      y: circle.center.y,
-      radius: circle.radius,
-      startAngle: 0,
-      endAngle: 6.283185307179586,
-      anticlockwise: true,
-    };
+      const expected = {
+        height: rect.height,
+        width: rect.width,
+        ...rect.center,
+      };
 
-    if (!ctx) throw new Error("context was not mocked");
+      if (!ctx) throw new Error("context was not mocked");
 
-    drawShape(ctx, circle);
+      drawShape(ctx, rect);
 
-    // @ts-expect-error these methods on the mock dont exist on the real rendering context
-    const events: any[] = ctx.__getEvents();
+      // @ts-expect-error these methods on the mock dont exist on the real rendering context
+      const events: any[] = ctx.__getEvents();
 
-    expect(events[2]["props"]).toStrictEqual(expected);
-  });
+      expect(events[1]["props"]).toStrictEqual(expected);
+    });
 
-  it("calls fill method last when drawing a circle", () => {
-    expect.assertions(1);
+    it("calls ctx twice when drawing a rectangle", () => {
+      expect.assertions(1);
 
-    if (!ctx) throw new Error("context was not mocked");
+      if (!ctx) throw new Error("context was not mocked");
 
-    drawShape(ctx, circle);
+      drawShape(ctx, rect);
 
-    // @ts-expect-error these methods on the mock dont exist on the real rendering context
-    const events: any[] = ctx.__getEvents();
+      // @ts-expect-error these methods on the mock dont exist on the real rendering context
+      const events: any[] = ctx.__getEvents();
 
-    expect(events[events.length - 1]["type"]).toStrictEqual("fill");
-  });
-
-  it("calls fillRect method second when drawing a rect", () => {
-    expect.assertions(1);
-
-    if (!ctx) throw new Error("context was not mocked");
-
-    drawShape(ctx, rect);
-
-    // @ts-expect-error these methods on the mock dont exist on the real rendering context
-    const events: any[] = ctx.__getEvents();
-
-    expect(events[1]["type"]).toStrictEqual("fillRect");
-  });
-
-  it("calls fillRect method with expected props when drawing a rect", () => {
-    expect.assertions(1);
-
-    const expected = {
-      height: rect.height,
-      width: rect.width,
-      ...rect.center,
-    };
-
-    if (!ctx) throw new Error("context was not mocked");
-
-    drawShape(ctx, rect);
-
-    // @ts-expect-error these methods on the mock dont exist on the real rendering context
-    const events: any[] = ctx.__getEvents();
-
-    expect(events[1]["props"]).toStrictEqual(expected);
-  });
-
-  it("calls ctx twice when drawing a rectangle", () => {
-    expect.assertions(1);
-
-    if (!ctx) throw new Error("context was not mocked");
-
-    drawShape(ctx, rect);
-
-    // @ts-expect-error these methods on the mock dont exist on the real rendering context
-    const events: any[] = ctx.__getEvents();
-
-    expect(events).toHaveLength(2);
-  });
-
-  it("calls ctx four times when drawing a circle", () => {
-    expect.assertions(1);
-
-    if (!ctx) throw new Error("context was not mocked");
-
-    drawShape(ctx, circle);
-
-    // @ts-expect-error these methods on the mock dont exist on the real rendering context
-    const events: any[] = ctx.__getEvents();
-
-    expect(events).toHaveLength(4);
+      expect(events).toHaveLength(2);
+    });
   });
 });
 
 describe("selectShape", () => {
-  it("calls strokeStyle method first when selecting a shape", () => {
-    expect.assertions(1);
+  describe("circle", () => {
+    it("calls strokeStyle method first", () => {
+      expect.assertions(1);
 
-    if (!ctx) throw new Error("context was not mocked");
+      if (!ctx) throw new Error("context was not mocked");
 
-    selectShape(ctx, circle);
+      selectShape(ctx, circle);
 
-    // @ts-expect-error these methods on the mock dont exist on the real rendering context
-    const events: any[] = ctx.__getEvents();
+      // @ts-expect-error these methods on the mock dont exist on the real rendering context
+      const events: any[] = ctx.__getEvents();
 
-    expect(events[0]["type"]).toStrictEqual("strokeStyle");
+      expect(events[0]["type"]).toStrictEqual("strokeStyle");
+    });
+
+    it("calls lineWidth method second", () => {
+      expect.assertions(1);
+
+      if (!ctx) throw new Error("context was not mocked");
+
+      selectShape(ctx, circle);
+
+      // @ts-expect-error these methods on the mock dont exist on the real rendering context
+      const events: any[] = ctx.__getEvents();
+
+      expect(events[1]["type"]).toStrictEqual("lineWidth");
+    });
+
+    it("calls beginPath method third", () => {
+      expect.assertions(1);
+
+      if (!ctx) throw new Error("context was not mocked");
+
+      selectShape(ctx, circle);
+
+      // @ts-expect-error these methods on the mock dont exist on the real rendering context
+      const events: any[] = ctx.__getEvents();
+
+      expect(events[2]["type"]).toStrictEqual("beginPath");
+    });
+
+    it("calls arc method fourth", () => {
+      expect.assertions(1);
+
+      if (!ctx) throw new Error("context was not mocked");
+
+      selectShape(ctx, circle);
+
+      // @ts-expect-error these methods on the mock dont exist on the real rendering context
+      const events: any[] = ctx.__getEvents();
+
+      expect(events[3]["type"]).toStrictEqual("arc");
+    });
+
+    it("calls arc method with expected props", () => {
+      expect.assertions(1);
+
+      const expected = {
+        radius: circle.radius * 2,
+        x: circle.center.x,
+        y: circle.center.y,
+        startAngle: 0,
+        anticlockwise: true,
+        endAngle: 6.283185307179586,
+      };
+
+      if (!ctx) throw new Error("context was not mocked");
+
+      selectShape(ctx, circle);
+
+      // @ts-expect-error these methods on the mock dont exist on the real rendering context
+      const events: any[] = ctx.__getEvents();
+
+      expect(events[3]["props"]).toStrictEqual(expected);
+    });
+
+    it("calls stroke method last", () => {
+      expect.assertions(1);
+
+      if (!ctx) throw new Error("context was not mocked");
+
+      selectShape(ctx, circle);
+
+      // @ts-expect-error these methods on the mock dont exist on the real rendering context
+      const events: any[] = ctx.__getEvents();
+
+      expect(events[events.length - 1]["type"]).toStrictEqual("stroke");
+    });
+
+    it("calls context five times", () => {
+      expect.assertions(1);
+
+      if (!ctx) throw new Error("context was not mocked");
+
+      selectShape(ctx, circle);
+
+      // @ts-expect-error these methods on the mock dont exist on the real rendering context
+      const events: any[] = ctx.__getEvents();
+
+      expect(events).toHaveLength(5);
+    });
+  });
+
+  describe("rectangle", () => {
+    it("calls strokeStyle method first", () => {
+      expect.assertions(1);
+
+      if (!ctx) throw new Error("context was not mocked");
+
+      selectShape(ctx, rect);
+
+      // @ts-expect-error these methods on the mock dont exist on the real rendering context
+      const events: any[] = ctx.__getEvents();
+
+      expect(events[0]["type"]).toStrictEqual("strokeStyle");
+    });
+
+    it("calls lineWidth method second", () => {
+      expect.assertions(1);
+
+      if (!ctx) throw new Error("context was not mocked");
+
+      selectShape(ctx, rect);
+
+      // @ts-expect-error these methods on the mock dont exist on the real rendering context
+      const events: any[] = ctx.__getEvents();
+
+      expect(events[1]["type"]).toStrictEqual("lineWidth");
+    });
+
+    it("calls strokeRect method last", () => {
+      expect.assertions(1);
+
+      if (!ctx) throw new Error("context was not mocked");
+
+      selectShape(ctx, rect);
+
+      // @ts-expect-error these methods on the mock dont exist on the real rendering context
+      const events: any[] = ctx.__getEvents();
+
+      expect(events[events.length - 1]["type"]).toStrictEqual("strokeRect");
+    });
+
+    it("calls strokeRect method with expected props", () => {
+      expect.assertions(1);
+
+      const expected = {
+        height: rect.height,
+        width: rect.width,
+        ...rect.center,
+      };
+
+      if (!ctx) throw new Error("context was not mocked");
+
+      selectShape(ctx, rect);
+
+      // @ts-expect-error these methods on the mock dont exist on the real rendering context
+      const events: any[] = ctx.__getEvents();
+
+      expect(events[events.length - 1]["props"]).toStrictEqual(expected);
+    });
+
+    it("calls context thrice", () => {
+      expect.assertions(1);
+
+      if (!ctx) throw new Error("context was not mocked");
+
+      selectShape(ctx, rect);
+
+      // @ts-expect-error these methods on the mock dont exist on the real rendering context
+      const events: any[] = ctx.__getEvents();
+
+      expect(events).toHaveLength(3);
+    });
   });
 });
--- src/func/draw.ts
+++ src/func/draw.ts
@@ -1,5 +1,7 @@
 import { Shape } from "../shapes";
 
+const HIGHLIGHT_COLOR = "rgba(255, 251, 0, 0.7)";
+
 export const drawShape = (
   ctx: CanvasRenderingContext2D,
   { color, center: { x, y }, ...s }: Shape
@@ -16,4 +18,21 @@ export const drawShape = (
   }
 
   ctx.fillRect(x, y, s.width, s.height);
+};
+
+export const selectShape = (
+  ctx: CanvasRenderingContext2D,
+  { center: { x, y }, ...s }: Shape
+): void => {
+  ctx.strokeStyle = HIGHLIGHT_COLOR;
+  ctx.lineWidth = 10;
+
+  if ("radius" in s) {
+    ctx.beginPath();
+    ctx.arc(x, y, s.radius + 5, 0, Math.PI * 2, true);
+    ctx.stroke();
+    return;
+  }
+
+  ctx.strokeRect(x, y, s.width, s.height);
 };
