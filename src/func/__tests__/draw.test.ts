597dba0f896f80f64493f4ddbcde0f744c18ec37
haiturtle <haiturtle@haiturtle.com>
2022-04-28 20:04:41 -0400

    ADD: failing context call test

--- src/func/__tests__/draw.test.ts
+++ src/func/__tests__/draw.test.ts
@@ -4,5 +4,29 @@
 import { drawShape } from "../draw";
 
 describe("drawShape", () => {
-  it.todo("calls arc with circle properties when drawing a circle");
+  const circle = {
+    radius: 5,
+    center: {
+      x: 2,
+      y: 3,
+    },
+    color: "black",
+  };
+
+  it("calls context when drawing a shape", () => {
+    expect.assertions(1);
+
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+
+    // eslint-disable-next-line jest/no-conditional-in-test
+    if (!ctx) throw new Error("context was not mocked");
+
+    drawShape(ctx, circle);
+
+    //@ts-expect-error these methods on the mock dont exist on the real rendering context
+    const events: unknown[] = ctx.__getEvents();
+
+    expect(events.length).toBeGreaterThan(0);
+  });
 });
