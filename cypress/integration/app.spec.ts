70e52a388bfb64168120007b9d07bef12dbad538
haiturtle <haiturtle@haiturtle.com>
2022-04-28 23:49:23 -0400

    ADD: e2e test case for highlight rect

--- cypress/integration/app.spec.ts
+++ cypress/integration/app.spec.ts
@@ -1,10 +1,6 @@
 describe("The App", () => {
-  it("successfully loads", () => {
-    cy.visit("http://localhost:3000"); // change URL to match your dev URL
-  });
-
   it("highlights a circle on click of circle", () => {
-    cy.visit("http://localhost:3000"); // change URL to match your dev URL
+    cy.visit("http://localhost:3000");
     cy.findByRole("button", { name: /add circle/i }).click();
 
     cy.findByRole("img", {
@@ -12,5 +8,16 @@ describe("The App", () => {
     }).click(75, 75);
 
     cy.compareSnapshot("circle drawn", { errorThreshold: 0.1 });
+  });
+
+  it("highlights a rect on click of rect", () => {
+    cy.visit("http://localhost:3000");
+    cy.findByRole("button", { name: /add rectangle/i }).click();
+
+    cy.findByRole("img", {
+      name: /draw shapes here/i,
+    }).click(40, 50);
+
+    cy.compareSnapshot("rect drawn", { errorThreshold: 0.1 });
   });
 });
