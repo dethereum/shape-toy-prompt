ecdb92f1e1feb0d5d9f18a451d839a60af3502fa
haiturtle <haiturtle@haiturtle.com>
2022-04-28 20:30:25 -0400

    ADD: a bunch of tests cases to the circle branch of drawShape fxn

--- src/func/__tests__/draw.test.ts
+++ src/func/__tests__/draw.test.ts
@@ -13,7 +13,7 @@ describe("drawShape", () => {
     color: "black",
   };
 
-  it("calls context when drawing a circle", () => {
+  it("calls fillStyle method first when drawing a circle", () => {
     expect.assertions(1);
 
     const canvas = document.createElement("canvas");
@@ -24,9 +24,91 @@ describe("drawShape", () => {
 
     drawShape(ctx, circle);
 
-    //@ts-expect-error these methods on the mock dont exist on the real rendering context
-    const events: unknown[] = ctx.__getEvents();
+    // @ts-expect-error these methods on the mock dont exist on the real rendering context
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const events: any[] = ctx.__getEvents();
 
-    expect(events.length).toBeGreaterThan(0);
+    expect(events[0]["type"]).toStrictEqual("fillStyle");
+  });
+
+  it("calls beginPath method second when drawing a circle", () => {
+    expect.assertions(1);
+
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+
+    // eslint-disable-next-line jest/no-conditional-in-test
+    if (!ctx) throw new Error("context was not mocked");
+
+    drawShape(ctx, circle);
+
+    // @ts-expect-error these methods on the mock dont exist on the real rendering context
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const events: any[] = ctx.__getEvents();
+
+    expect(events[1]["type"]).toStrictEqual("beginPath");
+  });
+
+  it("calls arc method third when drawing a circle", () => {
+    expect.assertions(1);
+
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+
+    // eslint-disable-next-line jest/no-conditional-in-test
+    if (!ctx) throw new Error("context was not mocked");
+
+    drawShape(ctx, circle);
+
+    // @ts-expect-error these methods on the mock dont exist on the real rendering context
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const events: any[] = ctx.__getEvents();
+
+    expect(events[2]["type"]).toStrictEqual("arc");
+  });
+
+  it("calls arc method with expected props when drawing a circle", () => {
+    expect.assertions(1);
+
+    const expected = {
+      x: circle.center.x,
+      y: circle.center.y,
+      radius: circle.radius,
+      startAngle: 0,
+      endAngle: 6.283185307179586,
+      anticlockwise: true,
+    };
+
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+
+    // eslint-disable-next-line jest/no-conditional-in-test
+    if (!ctx) throw new Error("context was not mocked");
+
+    drawShape(ctx, circle);
+
+    // @ts-expect-error these methods on the mock dont exist on the real rendering context
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const events: any[] = ctx.__getEvents();
+
+    expect(events[2]["props"]).toStrictEqual(expected);
+  });
+
+  it("calls fill method last when drawing a circle", () => {
+    expect.assertions(1);
+
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+
+    // eslint-disable-next-line jest/no-conditional-in-test
+    if (!ctx) throw new Error("context was not mocked");
+
+    drawShape(ctx, circle);
+
+    // @ts-expect-error these methods on the mock dont exist on the real rendering context
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const events: any[] = ctx.__getEvents();
+
+    expect(events[events.length - 1]["type"]).toStrictEqual("fill");
   });
 });
