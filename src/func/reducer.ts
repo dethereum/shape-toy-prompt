62d20a05b49f7389c6ba9926d9ca497e64dff462
haiturtle <haiturtle@haiturtle.com>
2022-04-30 15:20:44 -0400

    ADD: cases and actions for all current functionality

--- src/func/reducer.ts
+++ src/func/reducer.ts
@@ -5,7 +5,36 @@ type AddAction = {
   payload: Shape;
 };
 
-export type RootAction = AddAction;
+type SelectAction = {
+  type: "SELECT";
+  payload: Shape;
+};
+
+type DeselectAction = {
+  type: "DESELECT";
+  payload: Shape;
+};
+
+type DeselectAllAction = {
+  type: "DESELECT_ALL";
+};
+
+type HighlightAction = {
+  type: "HIGHLIGHT";
+  payload: Shape;
+};
+
+type RemoveHighlightAction = {
+  type: "REMOVE_HIGHLIGHT";
+};
+
+export type RootAction =
+  | AddAction
+  | SelectAction
+  | DeselectAction
+  | DeselectAllAction
+  | HighlightAction
+  | RemoveHighlightAction;
 
 export type RootState = {
   entities: Record<string, Shape>;
@@ -24,6 +53,70 @@ export const reducer = (state: RootState, action: RootAction): RootState => {
           ...state.entities,
           [action.payload.id]: action.payload,
         },
+      };
+    case "SELECT":
+      return {
+        ...state,
+        entities: {
+          ...state.entities,
+          [action.payload.id]: {
+            ...action.payload,
+            isSelected: true,
+          },
+        },
+      };
+    case "DESELECT":
+      return {
+        ...state,
+        entities: {
+          ...state.entities,
+          [action.payload.id]: {
+            ...action.payload,
+            isSelected: false,
+          },
+        },
+      };
+    case "DESELECT_ALL":
+      return {
+        ...state,
+        selected: [],
+        entities: state.selected.reduce<Record<string, Shape>>((shapes, id) => {
+          const s = shapes[id];
+          if (!s) return shapes;
+
+          return {
+            ...shapes,
+            [id]: { ...s, isSelected: false },
+          };
+        }, state.entities),
+      };
+    case "HIGHLIGHT":
+      return {
+        ...state,
+        entities: {
+          ...state.entities,
+          [action.payload.id]: {
+            ...action.payload,
+            isHighlighted: true,
+          },
+        },
+        highlighted: action.payload.id,
+      };
+
+    case "REMOVE_HIGHLIGHT":
+      // eslint-disable-next-line no-case-declarations
+      const hs = state.entities[state.highlighted];
+      if (!hs) return state;
+      return {
+        ...state,
+        entities: {
+          ...state.entities,
+          [state.highlighted]: {
+            ...hs,
+            isHighlighted: false,
+          },
+        },
+        highlighted: "",
       };
     default:
       return state;
